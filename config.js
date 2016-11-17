7f730571ae5a1720a9e4a33b6066e86e48a9921a
Archakov06 <archakovv95@mail.ru>
2016-11-17 15:59:47 +0300

    Загрузка всех файлов

---
+++ config.js
@@ -0,0 +1,9 @@
+var config = {
+	'group_id' : '-110234774',
+	'tg_token' : 'ТОКЕН_ТЕЛЕГРАМ',
+	'vk_appId' : 'ID_ВК_ПРИЛОЖЕНИЯ',
+	'vk_secret' : 'СЕКРЕТНЫЙ_КЛЮЧ_ВК_ПРИЛОЖЕНИЯ',
+	'vk_token' : 'ВК_ТОКЕН',
+};
+
+module.exports = config;
